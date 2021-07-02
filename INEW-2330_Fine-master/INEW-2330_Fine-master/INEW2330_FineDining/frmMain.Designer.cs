60d1904ba8ee118856cf82608d994cf94b4cc743
stephenkolls <84876344+stephenkolls@users.noreply.github.com>
2021-07-02 16:09:11 -0500

    Add files via upload

---
+++ INEW-2330_Fine-master/INEW-2330_Fine-master/INEW2330_FineDining/frmMain.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace INEW2330_FineDining
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblExit = new System.Windows.Forms.Label();
+            this.lblMenu = new System.Windows.Forms.Label();
+            this.lblOrder = new System.Windows.Forms.Label();
+            this.lblAbout = new System.Windows.Forms.Label();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Transparent;
+            this.lblName.Font = new System.Drawing.Font("Baskerville Old Face", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.lblName.Location = new System.Drawing.Point(381, 375);
+            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(415, 57);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "GASTRONOMIE";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Baskerville Old Face", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label1.Location = new System.Drawing.Point(469, 442);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Fine Dining Restraunt";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Kunstler Script", 99.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label2.Location = new System.Drawing.Point(443, 169);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(313, 192);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "G";
+            // 
+            // lblExit
+            // 
+            this.lblExit.AutoSize = true;
+            this.lblExit.BackColor = System.Drawing.Color.Transparent;
+            this.lblExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblExit.Font = new System.Drawing.Font("Baskerville Old Face", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExit.ForeColor = System.Drawing.Color.SeaShell;
+            this.lblExit.Location = new System.Drawing.Point(1120, 11);
+            this.lblExit.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblExit.Name = "lblExit";
+            this.lblExit.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblExit.Size = new System.Drawing.Size(61, 31);
+            this.lblExit.TabIndex = 7;
+            this.lblExit.Text = "Exit";
+            this.lblExit.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblMenu
+            // 
+            this.lblMenu.AutoSize = true;
+            this.lblMenu.BackColor = System.Drawing.Color.Transparent;
+            this.lblMenu.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblMenu.Font = new System.Drawing.Font("Baskerville Old Face", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMenu.ForeColor = System.Drawing.Color.SeaShell;
+            this.lblMenu.Location = new System.Drawing.Point(791, 11);
+            this.lblMenu.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMenu.Name = "lblMenu";
+            this.lblMenu.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblMenu.Size = new System.Drawing.Size(82, 31);
+            this.lblMenu.TabIndex = 8;
+            this.lblMenu.Text = "Menu";
+            this.lblMenu.Click += new System.EventHandler(this.lblMenu_Click);
+            // 
+            // lblOrder
+            // 
+            this.lblOrder.AutoSize = true;
+            this.lblOrder.BackColor = System.Drawing.Color.Transparent;
+            this.lblOrder.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblOrder.Font = new System.Drawing.Font("Baskerville Old Face", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOrder.ForeColor = System.Drawing.Color.SeaShell;
+            this.lblOrder.Location = new System.Drawing.Point(884, 11);
+            this.lblOrder.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblOrder.Name = "lblOrder";
+            this.lblOrder.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblOrder.Size = new System.Drawing.Size(85, 31);
+            this.lblOrder.TabIndex = 9;
+            this.lblOrder.Text = "Order";
+            this.lblOrder.Click += new System.EventHandler(this.lblOrder_Click);
+            // 
+            // lblAbout
+            // 
+            this.lblAbout.AutoSize = true;
+            this.lblAbout.BackColor = System.Drawing.Color.Transparent;
+            this.lblAbout.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblAbout.Font = new System.Drawing.Font("Baskerville Old Face", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAbout.ForeColor = System.Drawing.Color.SeaShell;
+            this.lblAbout.Location = new System.Drawing.Point(980, 11);
+            this.lblAbout.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAbout.Name = "lblAbout";
+            this.lblAbout.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblAbout.Size = new System.Drawing.Size(127, 31);
+            this.lblAbout.TabIndex = 10;
+            this.lblAbout.Text = "About Us";
+            this.lblAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.BackColor = System.Drawing.Color.Transparent;
+            this.lblLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblLogin.Font = new System.Drawing.Font("Baskerville Old Face", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.ForeColor = System.Drawing.Color.SeaShell;
+            this.lblLogin.Location = new System.Drawing.Point(690, 11);
+            this.lblLogin.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblLogin.Size = new System.Drawing.Size(81, 31);
+            this.lblLogin.TabIndex = 11;
+            this.lblLogin.Text = "Login";
+            this.lblLogin.Click += new System.EventHandler(this.lblLogin_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::INEW2330_FineDining.Properties.Resources.jay_wennington_N_Y88TWmGwA_unsplash;
+            this.pictureBox1.Location = new System.Drawing.Point(-3, -4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1197, 743);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1192, 736);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.lblAbout);
+            this.Controls.Add(this.lblOrder);
+            this.Controls.Add(this.lblMenu);
+            this.Controls.Add(this.lblExit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblExit;
+        private System.Windows.Forms.Label lblMenu;
+        private System.Windows.Forms.Label lblOrder;
+        private System.Windows.Forms.Label lblAbout;
+        private System.Windows.Forms.Label lblLogin;
+    }
+}
+
